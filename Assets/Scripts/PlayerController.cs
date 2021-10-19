7596d594a3abc4c13a527593b3b587379728fc09
Eliachar Feig <eliacharfe@edu.hac.ac.il>
2021-10-19 19:12:17 +0300

    1st Upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float torqueAmount = 1f;
+    Rigidbody2D rg2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rg2d.AddTorque(torqueAmount);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rg2d.AddTorque(-torqueAmount);
+        }
+    }
+}
